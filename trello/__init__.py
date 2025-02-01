05bee3147ff2887716d8ef5df403edd42635f61e
killerbite95 <88723065+killerbite95@users.noreply.github.com>
2025-02-01 19:02:22 +0100

    update

--- trello/__init__.py
+++ trello/__init__.py
@@ -1,4 +1,4 @@
 from .trello import TrelloCog
 
-def setup(bot):
-    bot.add_cog(TrelloCog(bot))
+async def setup(bot):
+    await bot.add_cog(TrelloCog(bot))
--- trello/trello.py
+++ trello/trello.py
@@ -282,5 +282,5 @@ class TrelloCog(commands.Cog):
         await self.bot.wait_until_ready()
 
 
-def setup(bot):
-    bot.add_cog(TrelloCog(bot))
+async def setup(bot):
+    await bot.add_cog(TrelloCog(bot))
