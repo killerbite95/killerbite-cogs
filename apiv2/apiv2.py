19de894fb2a31447e9c31b0cf2498107c7433dcc
killerbite95 <88723065+killerbite95@users.noreply.github.com>
2026-04-15 23:55:13 +0200

    refactor(apiv2): rename phase8.py -> community.py

--- apiv2/apiv2.py
+++ apiv2/apiv2.py
@@ -28,7 +28,7 @@ from .routes.webhooks import register_routes as register_webhook_routes
 from .routes.docs import register_routes as register_docs_routes
 from .routes.economy import register_routes as register_economy_routes
 from .routes.warnings_modlog import register_routes as register_warnings_modlog_routes
-from .routes.phase8 import register_routes as register_phase8_routes
+from .routes.community import register_routes as register_community_routes
 
 logger = logging.getLogger("red.killerbite95.apiv2")
 
@@ -102,7 +102,7 @@ class APIv2(commands.Cog):
         register_webhook_routes(self._app)
         register_economy_routes(self._app)
         register_warnings_modlog_routes(self._app)
-        register_phase8_routes(self._app)
+        register_community_routes(self._app)
 
         # Register external cog routes (@api_route)
         for cog_name, routes in self._external_routes.items():
